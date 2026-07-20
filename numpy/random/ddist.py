5319e754b610ac379301d54ffca714e0c689739e
Ishtiaq Ahmed <136582839+dev-ishtiaq@users.noreply.github.com>
2026-07-20 15:55:55 +0600

    random

---
+++ numpy/random/ddist.py
@@ -0,0 +1,9 @@
+from numpy import random
+x1 = random.choice([3, 5, 7, 9], p=[0.1, 0.3, 0.6, 0.0], size=(100))
+print(x1)
+
+# from numpy import random
+
+# x = random.choice([3, 5, 7, 9], p=[0.1, 0.3, 0.6, 0.0], size=(100))
+
+# print(x)
---
+++ numpy/random/intro.py
@@ -0,0 +1,17 @@
+from numpy import random
+
+# x = random.randint(100)
+# y= random.rand()
+# print(x)
+# print(y)
+
+# z = random.randint(100, size = (5))
+# za = random.randint(100, size = (3,5))
+# # print(z)
+# print(za)
+
+
+a = random.choice([3,5,8,9], size=(3,5))
+print(a)
+
+
