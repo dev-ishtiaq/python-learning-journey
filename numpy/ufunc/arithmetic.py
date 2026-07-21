3190d07981934c413d75b7b37330d2ea5c86bc3c
Ishtiaq Ahmed <136582839+dev-ishtiaq@users.noreply.github.com>
2026-07-21 16:05:15 +0600

    power

--- numpy/ufunc/arithmetic.py
+++ numpy/ufunc/arithmetic.py
@@ -9,10 +9,17 @@ import numpy as np
 # print(newarr)
 
 # ----------Subtraction---------
-arr1 = np.array([10, 11, 12, 13, 14, 15])
-arr2 = np.array([20, 21, 22, 23, 24, 25])
+# arr1 = np.array([10, 11, 12, 13, 14, 15])
+# arr2 = np.array([20, 21, 22, 23, 24, 25])
 
 
-newarr = np.subtract(arr1, arr2)
-print(newarr)
+# newarr = np.subtract(arr1, arr2)
+# print(newarr)
+
+# ---------Power---------
+# arr1 = ([5,10,20])
+# arr2 = ([2,3,2])
+
+# poarr = np.power(arr1, arr2)
+# print(poarr)
 
