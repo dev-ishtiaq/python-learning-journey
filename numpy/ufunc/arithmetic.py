1c4bef152e3678ceca53930497b88479ef72c9ae
Ishtiaq Ahmed <136582839+dev-ishtiaq@users.noreply.github.com>
2026-07-21 15:59:39 +0600

    Addition & Subtraction

--- numpy/ufunc/arithmetic.py
+++ numpy/ufunc/arithmetic.py
@@ -7,3 +7,12 @@ import numpy as np
 
 # newarr = np.add(arr1, arr2)
 # print(newarr)
+
+# ----------Subtraction---------
+arr1 = np.array([10, 11, 12, 13, 14, 15])
+arr2 = np.array([20, 21, 22, 23, 24, 25])
+
+
+newarr = np.subtract(arr1, arr2)
+print(newarr)
+
