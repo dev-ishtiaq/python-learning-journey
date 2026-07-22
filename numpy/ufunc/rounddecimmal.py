6f4c88e9f81cc0befc80c8d1a3ca6e41098244c9
Ishtiaq Ahmed <136582839+dev-ishtiaq@users.noreply.github.com>
2026-07-22 11:31:36 +0600

    decimal

---
+++ numpy/ufunc/rounddecimmal.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# --------Truncation------
+# arr = np.trunc([-1.4005, 2.548])
+# print(arr)
+
+# # -----fix------
+# arr2 = np.fix([-1.4005, 2.548])
+# print(arr2)
+
+
+# ---------Rounding---------
+# arr = np.around(3.2548, 2)
+
+# print(arr)
+
+# -------Floor-------
+arr1 = np.floor([-3.1548, 3.1684])
+
+arr2 = np.ceil([-3.1548, 3.1684])
+
+print(arr1)
+print(arr2)
