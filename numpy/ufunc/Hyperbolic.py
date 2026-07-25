2a735b3e2b83456dae6a11c0d9162812dfe83d81
Ishtiaq Ahmed <136582839+dev-ishtiaq@users.noreply.github.com>
2026-07-25 17:00:45 +0600

    Hyperbolic

---
+++ numpy/ufunc/Hyperbolic.py
@@ -0,0 +1,22 @@
+import numpy as np
+x = np.sinh(np.pi/2)
+print(x)
+
+
+
+arr = np.array([np.pi/2, np.pi/3, np.pi/4, np.pi/5])
+
+x2 = np.cosh(arr)
+
+print(x2)
+
+
+x3 = np.arcsinh(1.0)
+
+print(x3)
+
+arr = np.array([0.1, 0.2, 0.5])
+
+x3 = np.arctanh(arr)
+
+print(x3)
